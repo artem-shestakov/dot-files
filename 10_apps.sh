e2d69c449982153c75083d55acdaa6dcb0c1bd0d
Artem Shestakov <artem.s.shestakov@yandex.ru>
2024-03-09 11:28:28 +0300

    Update shell script

--- 10_apps.sh
+++ 10_apps.sh
@@ -4,13 +4,13 @@ echo "Installing Ubuntu packages"
 sudo apt update
 sudo apt install -y \
     python3-pip \
-    python3.10-venv \
+    python3.11-venv \
     vim \
     libffi-dev \
     dnsutils \
     clamav \
+    clamav-daemon \
     clamtk \
-    golang-1.21 \
     sshpass \
     whois \
     tmux
@@ -29,17 +29,18 @@ pip3 install \
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 ## Add the repository to Apt sources:
 echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
 ## Instal Docker + plugins
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo apt-get install -y 
     docker-ce \
     docker-ce-cli\
@@ -53,12 +54,14 @@ sudo apt -y install \
     cpu-checker \
     libvirt-clients \
     libvirt-daemon \
-    qemu \
+    qemu-system \
     qemu-kvm \
     virt-manager
 
 # Minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+sudo mv ./minikube /usr/local/bin/minikube
 
 # Kind
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
@@ -73,10 +76,6 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 #PgAdmin
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
-sudo apt install pgadmin4
-
-# Create links
-sudo ln -s /usr/lib/go-1.21/bin/go /usr/local/bin/go
-sudo ln -s /usr/lib/go-1.21/bin/gofmt /usr/local/bin/gofmt
+sudo apt install pgadmin4 -y
 
 echo "💪 Finished"
